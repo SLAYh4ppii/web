1623a190194f877b4a1ded5ec416bea62698b033
h4ppii <privat.jonaspasxal@gmail.com>
2025-01-06 17:37:52 +0100

    renamed:    app/en/about-us/page.tsx -> app/[locale]/about-us/page.tsx

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -1,17 +1,54 @@
-import {NextIntlClientProvider, useMessages} from 'next-intl';
+"use client";
+import { Chakra_Petch, Inter } from "next/font/google";
+import "@/public/scss/style.scss";
+import "react-toastify/dist/ReactToastify.css";
+import "material-symbols";
+import { useEffect } from "react";
+import { Providers } from "@/redux/provider";
+// import dynamic from "next/dynamic";
 
-export default function LocaleLayout({
+// const AnimatedCursor = dynamic(() => import("react-animated-cursor"), {
+//   ssr: false,
+// });
+const inter = Inter({ subsets: ["latin"], variable: "--body-font" });
+const chakraPetch = Chakra_Petch({
+  subsets: ["latin"],
+  variable: "--head-font",
+  weight: ["300", "400", "500", "600", "700"],
+});
+
+export default function RootLayout({
   children,
-  params: {locale}
 }: {
   children: React.ReactNode;
-  params: {locale: string};
 }) {
-  const messages = useMessages();
- 
+  useEffect(() => {
+    import("bootstrap/dist/js/bootstrap");
+  }, []);
   return (
-    <NextIntlClientProvider locale={locale} messages={messages}>
-      {children}
-    </NextIntlClientProvider>
+    <html lang="en">
+      <head>
+        <title>RickedStudios</title>
+      </head>
+      <body className={`${chakraPetch.variable} ${inter.variable} `}>
+        {/* <AnimatedCursor
+          innerSize={6}
+          outerSize={35}
+          innerScale={1}
+          outerScale={1.3}
+          outerAlpha={0}
+          showSystemCursor
+          innerStyle={{
+            backgroundColor: "#3FCA90",
+            zIndex: 99999,
+          }}
+          outerStyle={{
+            border: "2px solid #3FCA90",
+            zIndex: 99999,
+          }}
+        /> */}
+        <Providers>{children}</Providers>
+      </body>
+    </html>
   );
 }
